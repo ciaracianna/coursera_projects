029b20fe01d2394dc05f92e41df5f9bb76b26dd1
ciaracianna <96592704+ciaracianna@users.noreply.github.com>
2022-10-26 09:46:34 -0400

    Update script.js

--- html-js-css/vftvk-Simple-Interest-Calculator-master/script.js
+++ html-js-css/vftvk-Simple-Interest-Calculator-master/script.js
@@ -1,16 +1,29 @@
-function compute()
+function computeInterest()
 {
-    p = document.getElementById("principal").value;
-
+    // Computes the interest from input values
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("result").innerHTML = `Interest : If you deposit <mark>${principal}</mark>,<br/>at an interest rate of <mark>${rate}</mark>,<br/>You will receive an amount of <mark>${interest}</mark>,<br/> in the year <mark>${year}</mark>`;
 }
         
 function updateRate() 
 {
+    // Translates the slider to a numeric value
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
+} 
+
+function validation() {
+    // Checks for valid principal input 
+    var principal = document.getElementById("principal").value;
+    var greaterThanZero = parseInt(principal) > 0;
+    if (!greaterThanZero) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+
 }
